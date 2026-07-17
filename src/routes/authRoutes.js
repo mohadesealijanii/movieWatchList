5d9520d456a5485f470bc4bd65d6ff4342246e03
Mahdi <Mahdijoudaki1@gmail.com>
2026-07-17 20:09:10 +0330

    firat

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const router = express.Router();
+
+
+router.get("/register", register)
+
+export default router;
---
+++ src/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { config } from "dotenv";
+import movieRoutes from "./routes/movieRoutes.js";
+import { connectDB, disconnectDB } from "./config/db.js";
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.use("/movies", movieRoutes);
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection:", err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+process.on("SIGTERM", (err) => {
+  console.error("SIGTERM received:", err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
